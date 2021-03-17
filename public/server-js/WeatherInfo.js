766d685e96f38294f9b8f2eb002da80ed2355871
SEONKYU\difke <somedaycode@gmail.com>
2021-03-17 23:10:24 +0900

    add: weatherInfo.js 시간계산 함수 추가
    기상청 api basetime은 3시간 간격으로 달라진다.
    02:00 부터의 모든 데이터를 가져올 필요는 없으므로 지금 시간과 가장 가까운 api 데이터를 요청한다.

--- public/server-js/WeatherInfo.js
+++ public/server-js/WeatherInfo.js
@@ -9,9 +9,21 @@ const writefile = (fileData) => {
   });
 };
 
+const getBaseTime = (hour) => {
+  //- Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+  //- API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
+  if (hour === '00') hour = 24;
+  else hour = Number(hour);
+  const baseTime = String(hour - ((hour + 1) % 3));
+  if (baseTime.length === 1) baseTime + '0';
+  return baseTime + '00';
+};
+
 const requestWeatherInfo = (grid) => {
   moment.tz.setDefault('Asia/Seoul');
   const date = moment().format('YYYYMMDD');
+  const time = moment().format('HH');
+  const baseTime = getBaseTime(time);
 
   const callbackURL =
     'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?';
@@ -21,7 +33,7 @@ const requestWeatherInfo = (grid) => {
   const numOfRows = 'numOfRows=999';
   const dataType = 'dataType=JSON';
   const base_date = `base_date=${date}`;
-  const base_time = `base_time=0500`;
+  const base_time = `base_time=${baseTime}`;
   const nx = `nx=${grid.x}`;
   const ny = `ny=${grid.y}`;
   const URL = `${callbackURL}${serviceKey}&${pageNo}&${numOfRows}&${dataType}&${base_date}&${base_time}&${nx}&${ny}`;
