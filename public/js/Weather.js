33f846d10a58fe56073dc38ccdf46e4d27dfc033
SEONKYU\difke <somedaycode@gmail.com>
2021-03-18 02:21:26 +0900

    add: 서버에 저장된 날씨 정보를 fetch 한다.
    하늘 상태와 현재 온도를 가져온다

--- public/js/Weather.js
+++ public/js/Weather.js
@@ -1,9 +1,18 @@
 import { _ } from './util.js';
-
+/*
+ * 맑음	 0 ～ 5
+ * 구름많음	 6 ～ 8
+ * 흐림	 9 ～ 10
+ */
 class Weather {
   constructor(target) {
     this.init();
     this.COORD = 'COORD';
+    this.weatherInfo;
+    this.code = {
+      temperature: 'T1H',
+      sky: 'SKY',
+    };
   }
 
   init() {
@@ -50,13 +59,50 @@ class Weather {
   }
 
   async fetchData() {
-    const res = await fetch('data')
-      .then((raw) => raw.json())
-      .then((res) => res.response.body);
-    const weatherData = await res.items.item;
-    const test = Array.from(weatherData).filter((v) => v.category === 'POP');
-    console.log(test);
+    const cacheName = 'weatherCache';
+    const cacheStorage = await caches.open(cacheName);
+    const cachedData = await cacheStorage.match('weatherData');
+    if (cachedData) {
+      const data = await cachedData.json();
+      this.getWeatherInfoList(data);
+    } else {
+      const data = await fetch('/data').then((res) => {
+        let resClone = res.clone();
+        cacheStorage.put(cacheName, resClone);
+        return res.json();
+      });
+      this.getWeatherInfoList(data);
+    }
+  }
+
+  async getWeatherInfoList(responseData) {
+    const data = await responseData.response.body;
+    const weatherData = await data.items.item;
+    this.weatherInfo = Array.from(weatherData);
+    const currentTemp = this.getWeatherForcastValue(this.code.temperature);
+    const currentSky = this.getWeatherForcastValue(this.code.sky);
+    console.log(currentTemp, currentSky);
+  }
+
+  getWeatherForcastValue(code) {
+    const weatherForcastValue = this.weatherInfo
+      .filter((weather) => weather.category === code) //
+      .reduce((prev, curr) => {
+        return this.getCloseTimeFromNow(prev, curr);
+      }).fcstValue;
+    return weatherForcastValue;
+  }
+
+  getCloseTimeFromNow(prev, curr) {
+    if (prev.fcstTime === '0000') {
+      prev.fcstTime = '2400';
+    }
+    return prev.fcstTime > curr.fcstTime ? curr : prev;
   }
 }
 
+// 온도, 날씨상태 구했다.
+// 네이버 api로 주소가져오자
+// 혹은 카카오 지도
+// 하....
 export { Weather };
