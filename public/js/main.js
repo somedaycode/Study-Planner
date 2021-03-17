fdd9b64b466ef41952686d792033916d135389d5
SEONKYU\difke <somedaycode@gmail.com>
2021-03-17 15:57:39 +0900

    fix: Weather class 추가 및 수정

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
 import { _ } from './util.js';
 import { Menu } from './Menu.js';
+import { Weather } from './Weather.js';
 
 const DOMTargets = {
   menu: _.$('.menu'),
   menuBtn: _.$('.main-view-btn'),
 };
 
-const main = () => {
+const main = async () => {
   new Menu(DOMTargets);
+  new Weather(DOMTargets);
 };
 
 _.on(window, 'DOMContentLoaded', main);
