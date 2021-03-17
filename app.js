bdf6eb721acc8303883f20af825bfffefe29d4ef
SEONKYU\difke <somedaycode@gmail.com>
2021-03-17 22:27:58 +0900

    add: json data를 저장할 라우터 추가

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const dataRouter = require('./routes/data');
 
 const app = express();
 
@@ -13,14 +15,16 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/data', dataRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
