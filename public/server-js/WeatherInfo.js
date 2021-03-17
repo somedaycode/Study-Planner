a74c1b5d60a04a8d75b81d6e36b667a44c890406
SEONKYU\difke <somedaycode@gmail.com>
2021-03-18 02:21:54 +0900

    add: 가장 가까운 시간을 가져오기 위한 시간 계산

--- public/server-js/WeatherInfo.js
+++ public/server-js/WeatherInfo.js
@@ -9,13 +9,13 @@ const writefile = (fileData) => {
   });
 };
 
+//- Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+//- API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
 const getBaseTime = (hour) => {
-  //- Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
-  //- API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
   if (hour === '00') hour = 24;
   else hour = Number(hour);
-  const baseTime = String(hour - ((hour + 1) % 3));
-  if (baseTime.length === 1) baseTime + '0';
+  let baseTime = String(hour - 1);
+  if (baseTime.length === 1) baseTime = '0' + baseTime;
   return baseTime + '00';
 };
 
@@ -26,17 +26,17 @@ const requestWeatherInfo = (grid) => {
   const baseTime = getBaseTime(time);
 
   const callbackURL =
-    'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?';
+    'http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtFcst';
   const serviceKey =
     'serviceKey=O%2Bj3FsaYtTiaBzr4QTmqGuNS%2Bgflc%2FHdaldkCNDayWirwSI1faD6aPPW6q7EhdQoCsri%2BAbx%2FfUHn2sibtx9Hg%3D%3D';
   const pageNo = 'pageNo=1';
-  const numOfRows = 'numOfRows=999';
+  const numOfRows = 'numOfRows=100';
   const dataType = 'dataType=JSON';
   const base_date = `base_date=${date}`;
   const base_time = `base_time=${baseTime}`;
   const nx = `nx=${grid.x}`;
   const ny = `ny=${grid.y}`;
-  const URL = `${callbackURL}${serviceKey}&${pageNo}&${numOfRows}&${dataType}&${base_date}&${base_time}&${nx}&${ny}`;
+  const URL = `${callbackURL}?${serviceKey}&${pageNo}&${numOfRows}&${dataType}&${base_date}&${base_time}&${nx}&${ny}`;
 
   request(URL, (err, res, body) => {
     const weatherData = body;
