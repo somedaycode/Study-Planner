40d8a7c25b053d44ca597c462d3fbefac6bf84cb
SEONKYU\difke <somedaycode@gmail.com>
2021-03-17 22:29:26 +0900

    add: http request PUT으로 인한 라우터 경로 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const coordConverter = require('../public/server-js/coordConverter');
+const requestWeatherInfo = require('../public/server-js/WeatherInfo');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index.html');
+});
+
+router.put('/', function (req, res, next) {
+  const { latitude, longitude } = req.body;
+  const coord = coordConverter('toXY', latitude, longitude);
+  requestWeatherInfo(coord);
 });
 
 module.exports = router;
