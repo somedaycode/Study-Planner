b1015cd94811364e7a7924112e24c5b9f6d0d1b5
SEONKYU\difke <somedaycode@gmail.com>
2021-03-18 18:04:49 +0900

    add: 타이머에 사용할 DOM target 추가

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
 import { _ } from './util.js';
 import { Menu } from './Menu.js';
 import { Weather } from './Weather.js';
+import { Timer } from './Timer.js';
 
 const DOMTargets = {
   menu: _.$('.menu'),
   menuBtn: _.$('.main-view-btn'),
+  timer: {
+    btn: _.$('.timer-btn'),
+    hours: _.$('.hour'),
+    minutes: _.$('.minute'),
+    seconds: _.$('.second'),
+    start: _.$('.btn-start'),
+    stop: _.$('.btn-stop'),
+  },
 };
 
 const main = () => {
   new Menu(DOMTargets);
   new Weather(DOMTargets);
+  new Timer(DOMTargets);
 };
 
 main();
